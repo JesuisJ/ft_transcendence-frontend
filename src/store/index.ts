cad9175293590979877a38c5476deff9f573bfd2
JesuisJ <sw02096@icloud.com>
2026-04-17 19:07:17 +0200

    feat: apiSlice baseQuery + Add auto reauth middleware

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { apiSlice } from './api/apiSlice'
 import authReducer from './slices/authSlice'
 import uiReducer from './slices/uiSlice'
 
 export const store = configureStore({
   reducer: {
-    auth: authReducer,   // authSlice 연결
-    ui: uiReducer,       // uiSlice 연결
+    auth: authReducer,
+    ui: uiReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,  // apiSlice 연결
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),  // RTK Query 미들웨어 추가
 })
 
-// 타입 자동 추출 (TypeScript용)
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
